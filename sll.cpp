28814906a2e8966bc63681951b62830cfbbfbdf5
dichotomyofame <127950890+dichotomyofame@users.noreply.github.com>
2023-07-08 18:36:52 +0530

    push_front ,  pop_back & pop_front completed

--- sll.cpp
+++ sll.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+using namespace std;
 class SLL
 {
     private:
@@ -39,16 +39,68 @@ class SLL
         }
     }
     void push_front(int data)
-    {}
+    {
+        this->count++;
+        if(!this->root) 
+        {
+            root=new SLL_Node();
+            root->data=data;
+        }
+        else
+        {
+            SLL_Node *t=new SLL_Node();
+            t->data=data;
+            SLL_Node *tmp=root;
+            t->next=root;
+            root=t;
+        }
+    }
     void pop_back()
-    {}
+    {
+        if(this->count==0) return;
+        if(this->count==1) 
+        {
+            delete root;
+            root=NULL;
+        }
+        else
+        {
+            SLL_Node *i=NULL,*j=NULL;
+            for(j=root;j->next!=NULL;i=j,j=j->next);
+            i->next=NULL;
+            delete j;
+        }
+        this->count--;
+    }
     void pop_front()
-    {}
+    {
+        if(this->count==0) return;
+        if(this->count==1) 
+        {
+            delete root;
+            root=NULL;
+        }
+        else
+        {
+            SLL_Node *t=root->next;
+            delete root;
+            root=t;
+        }
+        this->count--;
+    }
     void insertByIndex(int data, int index)
     {}
 };
 
 int main()
 {
+    SLL tmp;
+    tmp.push_back(1);
+    tmp.push_back(2);
+    tmp.push_back(3);
+    tmp.print();
+    tmp.pop_front();
+    tmp.pop_back();
+    tmp.print();
     return 0;
 }
